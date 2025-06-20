d199e33328fd5424f84ff074c80d1d03d4e3962f
VireshShah05 <viresh.shah@hotwaxsystems.com>
2025-06-20 23:52:59 +0530

    Updated the address and google maps location

--- core/forms.py
+++ core/forms.py
@@ -2,6 +2,7 @@ from django import forms
 
 class ContactForm(forms.Form):
     name = forms.CharField(max_length =100, required = True)
+    phone_no = forms.CharField(max_length = 10, required = True)
     email = forms.EmailField(required = True)
     subject = forms.CharField(max_length = 150, required = False)
     message = forms.CharField(widget = forms.Textarea, required = True)
